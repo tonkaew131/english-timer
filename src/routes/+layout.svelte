8a64dd519632e546704befcbdb68b02afb286e23
tonkaew131 <nongtonkaew@gmail.com>
2023-08-17 22:48:59 +0700

    Added SEO

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,5 +4,15 @@
 	import '../app.postcss';
 </script>
 
+<svelte:head>
+	<title>English I Conversation test Timer</title>
+
+	<meta
+		name="description"
+		content="English I Conversation test Timer made with love ❤ by @tonkaew131"
+	/>
+	<meta name="author" content="tonkaew131" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+</svelte:head>
 
 <slot />
